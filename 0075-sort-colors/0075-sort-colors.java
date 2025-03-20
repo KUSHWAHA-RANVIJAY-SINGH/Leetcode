e639bdcb23f5642b508d6234a9f674a93edad268
Ranvijay Singh <143722140+KUSHWAHA-RANVIJAY-SINGH@users.noreply.github.com>
2025-03-20 18:56:51 +0530

    Time: 0 ms (100%), Space: 42 MB (65.04%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,25 +1,38 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int n = nums.length;
-        int i = 0;
-        int j = 0;
-        int k = n - 1;
+        // int n = nums.length;
+        // int [] count = new int[3];
 
-        while (j <= k) {
-            if (nums[j] == 1) {
-                j++;
-            } else if (nums[j] == 2) {
-                int temp = nums[j];
-                nums[j] = nums[k];
-                nums[k] = temp;
-                k--;
-            } else {
-                int temp = nums[j];
-                nums[j] = nums[i];
-                nums[i] = temp;
-                i++;
-                j++;
+        // for(int num : nums){
+        //     count[num]++;
+        // }
+        
+        // int index = 0;
+        // for(int i=0;i<count[0];i++){
+        //     nums[index++] =0;
+        // }
+        // for(int i=0;i<count[1];i++){
+        //     nums[index++] =1;
+        // }
+        // for(int i=0;i<count[2];i++){
+        //     nums[index++] =2;
+        // }   //Brute Froce Solution 
+
+        int low =0 ,mid =0, high = nums.length -1;
+        while(mid <= high){
+            if(nums[mid]==0){
+                swap(nums,low++,mid++);
+            }else if(nums[mid] ==1){
+                mid++;
+            }else{
+                swap(nums,mid,high--);
             }
         }
+       
     }
+     private void swap(int[] nums,int i,int j){
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+        }
 }
